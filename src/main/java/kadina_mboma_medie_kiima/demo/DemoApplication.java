7639c9cf9556bd6fe79b9cc27bcc92b63e4a4252
Africanboy Kiima <fadhilkiima@gmail.com>
2022-10-06 17:11:52 +0200

    started the springboot app project

---
+++ src/main/java/kadina_mboma_medie_kiima/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package kadina_mboma_medie_kiima.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kadina_mboma_medie_kiima/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kadina_mboma_medie_kiima.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
