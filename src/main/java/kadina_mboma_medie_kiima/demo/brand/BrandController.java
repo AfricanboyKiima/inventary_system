7b3c8e0cf46c0fc19cb4d79bd0528953f40d2bc8
Africanboy Kiima <fadhilkiima@gmail.com>
2022-10-13 15:25:08 +0200

    Worked on the brand feature so we can now see the list of brands

--- src/main/java/kadina_mboma_medie_kiima/demo/brand/BrandController.java
+++ src/main/java/kadina_mboma_medie_kiima/demo/brand/BrandController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class BrandController {
+
     @Autowired
     private BrandRepository brandRepo;
 
@@ -21,10 +23,23 @@ public class BrandController {
     @GetMapping("/brands/new")
     public String showCreateNewBrandForm(Model model){
         List<Category> listCategories =  categoryRepo.findAll();
+
         model.addAttribute("listCategories",listCategories);
         model.addAttribute("brand",new Brand());
 
         return "brand_form";
     }
 
+    @PostMapping("/brands/save")
+    public String saveBrand(Brand brand){
+        brandRepo.save(brand);
+
+        return "redirect:/";
+    }
+    @GetMapping("/brands")
+    public String listBrands(Model model){
+        List<Brand> listBrands = brandRepo.findAll();
+        model.addAttribute("listBrands", listBrands);
+        return "brands";
+    }
 }
